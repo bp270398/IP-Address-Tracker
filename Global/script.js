82d905d59bb21b26ae2849191f954539cbfa538b
Beata <68647262+bp270398@users.noreply.github.com>
2022-03-18 14:34:09 +0100

    Update script.js

--- Global/script.js
+++ Global/script.js
@@ -1,68 +1,6 @@
-/*
- * Using:
- * IP Geolocation API: https://app.abstractapi.com/api/ip-geolocation/documentation
- * IP API: https://ip-api.com/docs/api:json
- * Maps JavaScript API: https://developers.google.com/maps/documentation/javascript
- *
- */
+
 var data;
 $(function () {
-  // $.getJSON(
-  //   "https://ipgeolocation.abstractapi.com/v1/?api_key=1331725a33da4ae4bc6674a87e8c3c9b",
-  //   function (data) {
-  //     $("#ipAddressInput").val(data.ip_address);
-  //     $("#ipAddress").text(data.ip_address);
-  //     $("#coordinates").text(data.latitude + ", " + data.longitude);
-  //     $("#city").text(data.city);
-  //     $("#country").text(data.country);
-  //     $("#flag img").attr("src", data.flag.png);
-  //     $("#continent").text(data.continent);
-  //     $("#timezone").text(data.timezone.name);
-  //     $("#isp").text(data.connection.isp_name);
-  //     //Google Maps
-  //     function initMap() {
-  //       const uluru = { lat: data.latitude, lng: data.longitude };
-  //       const map = new google.maps.Map(document.getElementById("map"), {
-  //         zoom: 6,
-  //         center: uluru,
-  //       });
-  //       const marker = new google.maps.Marker({
-  //         position: uluru,
-  //         map: map,
-  //       });
-  //     }
-  //     initMap();
-  //   }
-  // );
-  // $("#ipAddressInput").on("change", function () {
-  //   $.getJSON(
-  //     "https://ipgeolocation.abstractapi.com/v1/?api_key=1331725a33da4ae4bc6674a87e8c3c9b & ip_address =" +
-  //       $("#ipAddressInput").val(),
-  //     function (data) {
-  //       $("#ipAddress").text(data.ip_address);
-  //       $("#coordinates").text(data.latitude + ", " + data.longitude);
-  //       $("#city").text(data.city);
-  //       $("#country").text(data.country);
-  //       $("#flag img").attr("src", data.flag.png);
-  //       $("#continent").text(data.continent);
-  //       $("#timezone").text(data.timezone.name);
-  //       $("#isp").text(data.connection.isp_name);
-  //       //Google Maps
-  //       function initMap() {
-  //         const uluru = { lat: data.latitude, lng: data.longitude };
-  //         const map = new google.maps.Map(document.getElementById("map"), {
-  //           zoom: 5,
-  //           center: uluru,
-  //         });
-  //         const marker = new google.maps.Marker({
-  //           position: uluru,
-  //           map: map,
-  //         });
-  //       }
-  //       initMap();
-  //     }
-  //   );
-  // });
 
   $.getJSON("http://ip-api.com/json/", function (info) {
     data = info;
